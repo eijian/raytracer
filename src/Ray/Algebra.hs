569e32f758beacea18b8e6730a54d36ee5024a5f
eijian <akagi.eiji.us@gmail.com>
2015-04-10 23:18:28 +0900

    update

--- src/Ray/Algebra.hs
+++ src/Ray/Algebra.hs
@@ -6,7 +6,7 @@ module Ray.Algebra where
 
 import Data.Maybe
 
-nearlyZero = 0.00000001 :: Double
+nearly0 = 0.00000001 :: Double
 
 class (Show a, Eq a) => BasicMatrix a where
   madd :: a -> a -> a
@@ -14,30 +14,35 @@ class (Show a, Eq a) => BasicMatrix a where
   mscale :: Double -> a -> a
   mdiv   :: a -> Double -> a
   norm :: a -> Double
-  nealyEqual :: a -> a -> Bool
+  nearlyEqual :: a -> a -> Bool
 
 class (BasicMatrix a) => Vector a where
   dot :: a -> a -> Double
   normalize :: a -> Maybe a
-  normalize a = a `mdiv` (norm a)
+  normalize v = v `mdiv` (norm v)
   square :: a -> Double
-  square a = a `dot` a
+  square v = v `dot` v
 
 data Vector3 = Vector3 Double Double Double
 
 instance Show Vector3 where
-  show (Vector3 ax ay az) = "[" ++ (show ax) ++ "," ++ (show ay) ++ "," ++ (show az) ++ "]"
+  show (Vector3 ax ay az) = "[" ++ (show ax) ++
+                            "," ++ (show ay) ++
+                            "," ++ (show az) ++ "]"
 
 instance Eq Vector3 where
-  (==) (Vector3 ax ay az) (Vector3 bx by bz) = ax == bx && ay == by && az == bz
+  (==) (Vector3 ax ay az) (Vector3 bx by bz) = ax == bx &&
+                                               ay == by &&
+                                               az == bz
 
+{-
 instance Arbitrary Vector3 where
   arbitrary = do
     x <- arbitrary
     y <- arbitrary
     z <- arbitrary
     return $ Vector3 x y z
-
+-}
 
 instance BasicMatrix Vector3 where
   -- |
@@ -46,8 +51,9 @@ instance BasicMatrix Vector3 where
   -- >>> madd (Vector3 1 2 3) (Vector3 4 5 6)
   -- [5.0,7.0,9.0]
   --
-  madd :: Vector3 -> Vector3 -> Vector3
-  madd (Vector3 ax ay az) (Vector3 bx by bz) = Vector3 (ax + bx) (ay + by) (az + bz)
+  madd (Vector3 ax ay az) (Vector3 bx by bz) = Vector3 (ax + bx)
+                                                       (ay + by)
+                                                       (az + bz)
 
   -- |
   -- vector substract
@@ -55,8 +61,9 @@ instance BasicMatrix Vector3 where
   -- >>> msub (Vector3 1 2 3) (Vector3 4 5 6)
   -- [-3.0,-3.0,-3.0]
   --
-  msub :: Vector3 -> Vector3 -> Vector3
-  msub (Vector3 ax ay az) (Vector3 bx by bz) = Vector3 (ax - bx) (ay - by) (az - bz)
+  msub (Vector3 ax ay az) (Vector3 bx by bz) = Vector3 (ax - bx)
+                                                       (ay - by)
+                                                       (az - bz)
 
   -- |
   -- vector scaling
@@ -64,43 +71,45 @@ instance BasicMatrix Vector3 where
   -- >>> mscale 4.0 (Vector3 1.1 2.1 3.1)
   -- [4.4,8.4,12.4]
   --
-  mscale :: Double -> Vector3 -> Vector3
   mscale s (Vector3 x y z) = Vector3 (s * x) (s * y) (s * z)
 
   -- |
   -- vector scalar division
   --
-  mdiv :: Vector3 -> Vector3 -> Vector3
   mdiv (Vector3 x y z) s = Vector3 (x / s) (y / s) (z / s)
 
   -- |
+  -- norm of vector
+  --
+  norm a = sqrt $ square a
+
+  -- |
+  -- nearly equal Zero
+  -- (.==.)
+  nearlyEqual (Vector3 ax ay az) (Vector3 bx by bz) = abs (ax - bx) < nearly0 &&
+                                                      abs (ay - by) < 0 &&
+                                                      abs (az - bz) < 0
+
+instance Vector Vector3 where
+  -- |
   -- dot vector
   --
-  dot :: Vector3 -> Vector3 -> Double
   dot (Vector3 ax ay az) (Vector3 bx by bz) = ax * bx + ay * by + az * bz
 
 -- |
 -- cross vector
 --
-
 cross :: Vector3 -> Vector3 -> Vector3
-cross (Vector3 ax ay az) (Vector3 bx by bz) = Vector3 (ay * bz - by * az) (az * bx - bz * ax) (ax * by - ay * bx)
+cross (Vector3 ax ay az) (Vector3 bx by bz) = Vector3 (ay * bz - by * az)
+                                                      (az * bx - bz * ax)
+                                                      (ax * by - ay * bx)
 
 -- |
 -- initialize Vector
 --
-
 initVec :: Double -> Double -> Double -> Vector3
 initVec x y z = Vector3 x y z
 
--- |
--- nearly equal Zero
---
-
-(.=.) :: Vector3 -> Vector3 -> Bool
-(.=.) (Vector3 ax ay az) (Vector3 bx by bz) = abs (ax - bx) < nearlyZero &&
-                                              abs (ay - by) < nearlyZero &&
-                                              abs (az - bz) < nearlyZero
 
 -- |
 -- element X axis of vector
