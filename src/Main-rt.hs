b6472b4c013b5cfc85c393f1033f5e0bcf25fd16
Eiji Akagi <akagi.eiji@gmail.com>
2015-05-23 00:57:06 +0900

    add Main for ray tracing

--- src/Main-rt.hs
+++ src/Main-rt.hs
@@ -6,10 +6,54 @@
 
 module Main where
 
+import qualified Data.Trees.KdTree
+
+eyepos = initPos 0 2 0)
+eyedir = ez3
+upper = ey3
+focus = 1.0
+
+eex = ex3
+eey = negate ey3
+stepx = 2.0 / 256
+stepy = 2.0 / 256
+origin = focus *> eyedir
+  + ((-1.0) *> eex + 0.5 * stepx)
+  - (1.0    *> eey + 0.5 * stepy)
+scrmap = [(y, x) | y <- [0..255], x <- [0..255]]  
+
+data PhotonInfo = PhotonInfo Wavelength Position3 Direction3
+
+instance Point PhotonInfo where
+  dimension _ = 3
+  coord 0 (PhotonInfo _ p _) = elemX p
+  coord 1 (PhotonInfo _ p _) = elemY p
+  coord 2 (PhotonInfo _ p _) = elemZ p
+  dist2 (PhotonInfo _ p1 _) (PhotonInfo _ p2 _) = square (p1 - p2)
 
 main :: IO ()
 main = do
+  (power, photonmap) <- readMap
+  let image = map traceCell scrmap
+  outputImage image
+
+readMap :: IO (Double, KdTree PhotonInfo)
+readMap = do
   np <- getLine
   pw <- getLine
-  pcs <- forM ([1..np]) $ \i -> do
-    getLine >>= (read :: PhotonCache)
+  pis <- forM ([1..np]) $ \i -> do
+    pc <- (getLine >>= (read :: PhotonCache))
+    return $ convertToInfo pc
+
+convertToInfo :: PhotonCache -> PhotonInfo
+convertToInfo (wl, (rp, rd)) = PhotonInfo wl rp (negate rd)
+
+traceCell :: (Int, Int) -> Radiance
+traceCell (y, x) = 
+
+generateRay :: (Double, Double) -> Maybe Ray
+generateRay (y, x) =
+  initRay eyepos (origin + ((stepx * x) *> eex) + ((stepy * y) *> eey))
+
+
+
