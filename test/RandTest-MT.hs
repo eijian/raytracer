22bb162cff7251a67d13a6834cb88bafe7003915
eijian <akagi.eiji.us@gmail.com>
2015-10-28 12:46:56 +0900

    update@of 20151028

--- test/RandTest-MT.hs
+++ test/RandTest-MT.hs
@@ -9,18 +9,16 @@ import Control.Monad
 import NumericPrelude
 import Ray.Algebra
 
-nvec = 10000000 :: Int
+plgt = PointLight (Color 1 1 1) 2.0 (Position3 0 0 0)
+nvec = 100 :: Int
 
 main :: IO ()
 main = do
-  vecs <- replicateM nvec generateRandomDir3
-  let v0 = foldl (+) (Vector3 0 0 0) vecs
-  putStrLn $ show v0
-  --mapM_ (putStrLn.show) vecs
+  forM_ [1..nvec] $ \i -> do
+    v <- generatePhoton plgt
+    putStrLn $ show v
 {--
   forM_ [1..5] $ \i -> do
     v <- generateRandomDir3
     putStrLn $ show v
 --}
-
-
